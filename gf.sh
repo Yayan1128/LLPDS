4c05a040fa0a184ed7e7c1cb772fec5993aa9fbc
Yayan1128 <95136847+Yayan1128@users.noreply.github.com>
2025-07-28 23:15:17 +0800

    Create gf.sh

---
+++ gf.sh
@@ -0,0 +1,50 @@
+#0.25%
+python train_semisup.py --dataset gf \
+    --num_labels 2 \
+    --apply_aug classmix \
+    --apply_icc \
+    --snapshot_path ./saved/resnet101/gf-0.25 \
+    --model_path './saved/resnet101/gf-0.25/model/model_best.pth' \
+    --binary_score_map_path "./saved/resnet101/gf-0.25/pred/" \
+    --gt_pred_seg_image_path "./saved/resnet101/gf-0.25/gt-pred/" \
+    --mask_path "./data/datagf/gf600-0.25/test/gtfine/" \
+    --log_dir './saved/resnet101/gf-0.25/test.log' \
+    --Test True \
+    --alpha 0.8 \
+    --memco 0.5 \
+    --output_dim 32 \
+    --Test True 
+
+##1%
+python train_semisup.py --dataset gf \
+    --num_labels 6 \
+    --apply_aug classmix \
+    --apply_icc \
+    --snapshot_path ./saved/resnet101/gf-1 \
+    --model_path './saved/resnet101/gf-1/model/model_best.pth' \
+    --binary_score_map_path "./saved/resnet101/gf-1/pred/" \
+    --gt_pred_seg_image_path "./saved/resnet101/gf-1/gt-pred/" \
+    --mask_path "./data/datagf/gf600-1/test/gtfine/" \
+    --log_dir './saved/resnet101/gf-1/test.log' \
+    --Test True \
+    --alpha 0.8 \
+    --memco 0.5 \
+    --output_dim 32 \
+    --Test True 
+
+# #2%
+python train_semisup.py --dataset gf \
+    --num_labels  12 \
+    --apply_aug classmix \
+    --apply_icc \
+    --snapshot_path ./saved/resnet101/gf-2 \
+    --model_path './saved/resnet101/gf-2/model/model_best.pth' \
+    --binary_score_map_path "./saved/resnet101/gf-2/pred/" \
+    --gt_pred_seg_image_path "./saved/resnet101/gf-2/gt-pred/" \
+    --mask_path "./data/datagf/gf600-2/test/gtfine/" \
+    --log_dir './saved/resnet101/gf-2/test.log' \
+    --Test True \
+    --alpha 0.7 \
+    --memco 0.5 \
+    --output_dim 64 \
+    --Test True 
